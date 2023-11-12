146f7281d64d00116407cbcab45d2fbfdd4e2aa5
Lougas <lougasndutra@yahoo.com.br>
2023-11-12 19:50:48 -0300

    Initial commit

---
+++ src/main/java/com/tcc/unicesumar/UnicesumarApplication.java
@@ -0,0 +1,14 @@
+package com.tcc.unicesumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicesumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicesumarApplication.class, args);
+	}
+erere
+
+}
---
+++ src/test/java/com/tcc/unicesumar/UnicesumarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.unicesumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicesumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
