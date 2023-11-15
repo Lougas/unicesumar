7dc19776e047f02c4fe9c35f637ea95a553bc45a
Lougas <lougasndutra@yahoo.com.br>
2023-11-15 20:05:02 -0300

    Banco de dados

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,13 @@
-package com.tcc.unicesumar;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UnicesumarApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UnicesumarApplication.class, args);
-
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
