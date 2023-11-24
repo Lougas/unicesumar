7f6e2a7297e60d95ffe73450a46991195fdd3072
Lougas <lougasndutra@yahoo.com.br>
2023-11-24 00:26:00 -0300

    Commit React Frontend

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> Lougas Dutra </h1>
+        <p>
+        Unicesumar <code> TCC 2 </code> App: make a wish- Faça um desejo.
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LND 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
